2112ef5a82ca81d3071ad96408ee57da1562425a
CondorW <rotsteiner@gmail.com>
2022-02-13 11:27:22 +0100

    Found a workaround to cart card styling, so that prices are aligned

--- src/Ui/ModalCard.js
+++ src/Ui/ModalCard.js
@@ -11,10 +11,15 @@ export default function ModalCard(props) {
   return (
     <div onClick={deleteItem} className="flex flex-row justify-center">
       <div className="bg-gray-100 w-10/12 m-1 rounded p-1 flex justify-between">
-        <p>{props.itemName}</p>
-        <p>{props.itemCount}</p>
-        <p>{props.itemPrice} €</p>
-        <p>X</p>
+        <div className="w-28">
+          <p>{props.itemName}</p>
+        </div>
+        <div className="w-20">
+          <p>{props.itemPrice} €</p>
+        </div>
+        <div>
+          <p>X</p>
+        </div>
       </div>
     </div>
   );
